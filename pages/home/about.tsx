f1c0868417a8951ce86e052888b3327c4eac717e
John Boyes <john@agilepathway.co.uk>
2024-02-23 05:20:38 +0000

    Format files with Biome

--- pages/home/about.tsx
+++ pages/home/about.tsx
@@ -2,14 +2,14 @@ import styles from "../../styles/Home.module.css";
 import Link from "next/link";
 
 export default function About() {
-  return (
-    <div className={styles.container}>
-      <main className={styles.main}>
-        <h1>About Page</h1>
-        <p className={styles.description}>
-          <Link href="/home">&larr; Go Back</Link>
-        </p>
-      </main>
-    </div>
-  );
+	return (
+		<div className={styles.container}>
+			<main className={styles.main}>
+				<h1>About Page</h1>
+				<p className={styles.description}>
+					<Link href="/home">&larr; Go Back</Link>
+				</p>
+			</main>
+		</div>
+	);
 }
